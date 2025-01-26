e9b4c1ce5f56731ed291b61d508ee852401816c8
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2025-01-26 16:05:04 +0100

    add: Users models

---
+++ Users.cs
@@ -0,0 +1,38 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace MentalApp
+{
+    internal class User
+    {
+        private static int _nextId = 1;
+        internal int Id { get; init; }
+        public DateTime RegistrationDate { get; init; }
+        public String Name { get; init; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public int NextLevelExperience { get; set; }
+
+        public User()
+        {
+            Id = _nextId++;
+            RegistrationDate = DateTime.Now;
+            Console.WriteLine("What is your name?");
+            var input = Console.ReadLine();
+            Name = string.IsNullOrWhiteSpace(input) ? "Anonymous" : input;
+            Level = 0;
+            Experience = 0;
+            NextLevelExperience = 30;
+        }
+
+        public void AddExperience()
+        {
+            Experience += 10;
+            if (Experience >= NextLevelExperience)
+            {
+                Experience -= NextLevelExperience;
+                NextLevelExperience += 10;
+                Level ++;
+            }
+        }
+    }
+}
