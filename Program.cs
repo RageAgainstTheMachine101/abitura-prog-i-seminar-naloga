4222c6921687582276ab8460422ca7141533663f
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2025-01-26 16:06:22 +0100

    add: Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using MentalApp;
+
+User user1 = new();
+User user2 = new();
+User user3 = new();
+
+Console.WriteLine($"{user1.Name}, {user1.Id}");
+Console.WriteLine($"{user2.Id}");
+Console.WriteLine($"{user3.Id}");
