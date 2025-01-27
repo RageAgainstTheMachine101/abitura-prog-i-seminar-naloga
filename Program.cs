ce2b0461ba351d3149ae75f3d6c56440cde61acc
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2025-01-27 21:40:09 +0100

    complete Program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using MentalApp;
-
-User user1 = new();
-User user2 = new();
-User user3 = new();
-
-Console.WriteLine($"{user1.Name}, {user1.Id}");
-Console.WriteLine($"{user2.Id}");
-Console.WriteLine($"{user3.Id}");
+﻿namespace MentalApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Menu.Run();
+        }
+    }
+}
