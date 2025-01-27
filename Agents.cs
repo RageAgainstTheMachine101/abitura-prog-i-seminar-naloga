0bf543938dc623316aafea9404c3c4dc17a9ef31
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2025-01-27 21:39:04 +0100

    add custom agents

--- Agents.cs
+++ Agents.cs
@@ -8,7 +8,7 @@ namespace MentalApp
         public string Prompt { get; init; }
         private static readonly string[] _greetings = { "Zdravo!", "Dober dan!!", "Oj!" };
 
-        BasicAgent(string name, string prompt)
+        public BasicAgent(string name, string prompt)
         {
             Id = _nextId++;
             Name = name;
@@ -19,6 +19,33 @@ namespace MentalApp
         {
             Random random = new();
             return _greetings[random.Next(_greetings.Length)];
+        }
+    }
+
+    class CompanionAgent : BasicAgent
+    {
+        private static readonly string[] _companionPhrases = { "I see... And what?", "How can I help you?" };
+
+        public CompanionAgent(string name, string prompt) : base(name, prompt) { }
+
+        public string PushPhrase()
+        {
+            Random random = new();
+            return _companionPhrases[random.Next(_companionPhrases.Length)];
+        }
+    }
+
+    class AnalystAgent : BasicAgent
+    {
+        private static readonly string[] _insightContents = { "You are a very honest person.", "You are so responsible." };
+
+        public AnalystAgent(string name, string prompt) : base(name, prompt) { }
+
+        public Insight CreateInsight(int userId, int conversationId)
+        {
+            Random random = new();
+            string content = _insightContents[random.Next(_insightContents.Length)];
+            return new Insight(userId, conversationId, content);
         }
     }
 }
